a955c7c8ac63e355ee0fc1d092e5e5facd4a0c74
ravindu <ravindulakshitha250@gmail.com>
2024-07-18 23:02:52 +0530

    update file strucutre

--- src/Context/genre-context.js
+++ src/Context/genre-context.js
@@ -1,36 +1,39 @@
-import { useState, useContext, createContext} from 'react'
+import { useState, useContext, createContext } from "react";
 
-const GenreContext = createContext()
+const GenreContext = createContext();
 
-let GenreProvider = ({children}) => 
-{
-    const [fictionCategoryCheckbox, setFictionCategoryCheckbox] = useState(true);
-    const [thrillerCategoryCheckbox, setThrillerCategoryCheckbox] = useState(true);
-    const [techCategoryCheckbox, setTechCategoryCheckbox] = useState(true);
-    const [philosophyCategoryCheckbox, setPhilosophyCategoryCheckbox] = useState(true);
-    const [romanceCategoryCheckbox, setRomanceCategoryCheckbox] = useState(true);
-    const [mangaCategoryCheckbox, setMangaCategoryCheckbox] = useState(true);
+let GenreProvider = ({ children }) => {
+  const [fictionCategoryCheckbox, setFictionCategoryCheckbox] = useState(true);
+  const [thrillerCategoryCheckbox, setThrillerCategoryCheckbox] =
+    useState(true);
+  const [techCategoryCheckbox, setTechCategoryCheckbox] = useState(true);
+  const [philosophyCategoryCheckbox, setPhilosophyCategoryCheckbox] =
+    useState(true);
+  const [romanceCategoryCheckbox, setRomanceCategoryCheckbox] = useState(true);
+  const [mangaCategoryCheckbox, setMangaCategoryCheckbox] = useState(true);
 
-    return (
-        <GenreContext.Provider value={{
-            fictionCategoryCheckbox, 
-            setFictionCategoryCheckbox,
-            thrillerCategoryCheckbox, 
-            setThrillerCategoryCheckbox,
-            techCategoryCheckbox, 
-            setTechCategoryCheckbox,
-            philosophyCategoryCheckbox, 
-            setPhilosophyCategoryCheckbox,
-            romanceCategoryCheckbox, 
-            setRomanceCategoryCheckbox,
-            mangaCategoryCheckbox, 
-            setMangaCategoryCheckbox,
-        }}>
-            {children}
-        </GenreContext.Provider>
-    )
-}
+  return (
+    <GenreContext.Provider
+      value={{
+        fictionCategoryCheckbox,
+        setFictionCategoryCheckbox,
+        thrillerCategoryCheckbox,
+        setThrillerCategoryCheckbox,
+        techCategoryCheckbox,
+        setTechCategoryCheckbox,
+        philosophyCategoryCheckbox,
+        setPhilosophyCategoryCheckbox,
+        romanceCategoryCheckbox,
+        setRomanceCategoryCheckbox,
+        mangaCategoryCheckbox,
+        setMangaCategoryCheckbox,
+      }}
+    >
+      {children}
+    </GenreContext.Provider>
+  );
+};
 
-let useGenre = () => useContext(GenreContext)
+let useGenre = () => useContext(GenreContext);
 
-export { GenreProvider, useGenre }
+export { GenreProvider, useGenre };
